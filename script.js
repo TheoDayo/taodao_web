6a285585572f200f3f71e25fca073500a225f1b5
TheoDayo <minhthang.dao@student.ufv.ca>
2024-01-06 16:34:31 -0800

    .

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const dishNameSelect = document.getElementById('dishName');
+    const timeSelect = document.getElementById('time');
+    const pickupOptions = ["Wednesday 5:00pm-6:00pm", "Thursday 5:00pm-6:00pm", "Friday 11:00am - 1:00pm", "Saturday 11:00am - 1:00pm"];
+    const deliveryOptions = ["Wednesday 7:00pm-8:00pm", "Thursday 7:00pm-8:00pm", "Friday 1:00pm - 2:00pm", "Saturday 1:00pm - 2:00pm"];
+
+    document.getElementsByName('deliveryType').forEach(function (radio) {
+        radio.addEventListener('change', function () {
+            updateOptions();
+        });
+    });
+
+    function updateOptions() {
+        const selectedDeliveryType = document.querySelector('input[name="deliveryType"]:checked').value;
+        const options = (selectedDeliveryType === 'pickup') ? pickupOptions : deliveryOptions;
+
+        // Clear previous options
+        timeSelect.innerHTML = '';
+
+        // Add new options
+        options.forEach(function (option) {
+            const optionElement = document.createElement('option');
+            optionElement.value = option;
+            optionElement.text = option;
+            timeSelect.add(optionElement);
+        });
+    }
+});
 
 var form = document.getElementById('sheetdb-form');
 form.addEventListener("submit", e => {
